efee9fe6b1b8752dfc440a7b8b4ca226bd36dba9
Stuwert <stuart@urback.net>
2020-12-05 23:18:05 -0700

    Add vertical to search

--- utilities/loadCustomCollections.js
+++ utilities/loadCustomCollections.js
@@ -1,22 +1,13 @@
 const customPlatforms = [ "Nintendo Switch", "Windows", "Mac", "Apple Arcade" , "iOS", "PS4", "PS5", "Xbox", "Android", "Tabletop", "Stadia", "Luna", "Wii", "Wii U", "PS3", "Nintendo DS", "PS Vita", "Xbox One", "Xbox Series S/X", "Steam" ];
-
+const verticals = ['piledriver'];
 
 module.exports = (eleventyConfig) => {
-  customPlatforms.forEach( (platform) => {
-    eleventyConfig.addCollection(platform, (collectionList) => {
-      return collectionList.getAll().filter( ({ data: { platforms } }) => {
-        if (!platforms) return false;
 
-        return platforms.includes(platform);
-      })
-    })
-  });
-
-  eleventyConfig.addCollection("platformList", function() {
-    return customPlatforms;
-  })
-
-  eleventyConfig.addCollection("tagList", function(collection) {
+/**
+ * Stuff about Tags
+ *  
+ */  
+eleventyConfig.addCollection("tagList", function(collection) {
     let tagSet = new Set();
     collection.getAll().forEach(function(item) {
       if( "tags" in item.data ) {
@@ -31,5 +22,34 @@ module.exports = (eleventyConfig) => {
     // returning an array in addCollection works in Eleventy 0.5.3
     return [...tagSet];
   });
+
+  /**
+   * Stuff about Platforms
+   */
+  customPlatforms.forEach( (platform) => {
+    eleventyConfig.addCollection(platform, (collectionList) => {
+      return collectionList.getAll().filter( ({ data: { platforms } }) => {
+        if (!platforms) return false;
+
+        return platforms.includes(platform);
+      })
+    })
+  });
+
+  eleventyConfig.addCollection("platformList", () => customPlatforms);
+  
+  /**
+   * Stuff About Verticals
+   */
+  
+  verticals.forEach( (vertical) => {
+    eleventyConfig.addCollection(vertical, (collectionList) => {
+      return collectionList.getAll().filter( ({ data: { vertical: articleInVertical } }) => {
+        return vertical === articleInVertical;
+      })
+    })
+  })
+
+  eleventyConfig.addCollection("verticalList", () => verticals);
 
 }
